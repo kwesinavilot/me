2de0ea515d44657265226a2a827511577525d6d2
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2023-11-22 09:47:26 +0000

    Added Cloudflare analytics to site

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,8 @@ export default class _Document extends Document {
         <body>
           <Main />
           <NextScript />
+
+          <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "7b693d048452436691b54a10a639f046"}'></script>
         </body>
       </Html>
     );
